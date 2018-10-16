dd6a017a27945d7bad158f24263fef7af9310fd9
tommydoang <tommydoang47@gmail.com>
2018-10-16 19:31:53 +0700

    Directing to ProductDetail

--- app/src/main/java/com/example/tomz/electroniccity/page/bn_home_content/tab1/Tab1ItemViewModel.java
+++ app/src/main/java/com/example/tomz/electroniccity/page/bn_home_content/tab1/Tab1ItemViewModel.java
@@ -1,45 +1,48 @@
 package com.example.tomz.electroniccity.page.bn_home_content.tab1;
 
+import android.content.Intent;
 import android.databinding.BindingAdapter;
 import android.databinding.ObservableField;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.tomz.electroniccity.data.model.api.products.tab1.DataProductTab1Response;
-
-import java.text.NumberFormat;
-import java.util.Locale;
+import com.example.tomz.electroniccity.page.details.checkout.ProductDetail;
+import com.example.tomz.electroniccity.utils.CommonUtils;
 
 public class Tab1ItemViewModel {
 
     public final ObservableField<String> imageUrl;
     public final ObservableField<String> nameProd;
     public final ObservableField<String> realPrice;
+    private DataProductTab1Response productTab1Response;
 
     public Tab1ItemViewModel(DataProductTab1Response dataModelProd) {
+        this.productTab1Response = dataModelProd;
         imageUrl = new ObservableField<>(dataModelProd.getImg_thumb());
         nameProd = new ObservableField<>(dataModelProd.getName_prod());
-        realPrice = new ObservableField<>(setCustomCurrency(dataModelProd.getReal_price()));
+        realPrice = new ObservableField<>(CommonUtils.setCustomCurrency(dataModelProd.getReal_price()));
     }
 
     @BindingAdapter({"imageViewBerak"})
     public static void loadImage(ImageView view, String url) {
-        Glide.with(view.getContext())
-                .load(url).into(view);
+        Glide.with(view.getContext()).load(url).into(view);
     }
 
     public View.OnClickListener onReadMoreClicked() {
-        return view -> Toast.makeText(view.getContext(), nameProd.get(), Toast.LENGTH_SHORT).show();
-    }
-
-    private String setCustomCurrency(String price){
-        String formattedCurr;
-        Locale localeID = new Locale("in", "ID");
-        NumberFormat currencyFormat = NumberFormat.getInstance(localeID);
-        formattedCurr = "Rp " + currencyFormat.format(Double.parseDouble(price));
-        return formattedCurr;
+        return (View view) -> {
+            Intent in = new Intent(view.getContext(), ProductDetail.class);
+            in.putExtra("TAG_ID_PROD",productTab1Response.getId_prod());
+            in.putExtra("TAG_IMG_PROD", productTab1Response.getImg_best());
+            in.putExtra("TAG_NAMA_PROD", productTab1Response.getName_prod());
+            in.putExtra("TAG_SKU_PROD", productTab1Response.getSku());
+            in.putExtra("TAG_NORMAL_PRICE", productTab1Response.getReal_price());
+            in.putExtra("TAG_SPC_PRICE", productTab1Response.getSpc_price());
+            in.putExtra("TAG_PROD_DESC", productTab1Response.getProduct_description()); //deskripsi produk
+            in.putExtra("TAG_LONG_DESC", productTab1Response.getLong_description()); //spesifikasi produk
+            view.getContext().startActivity(in);
+        };
     }
 
 }
