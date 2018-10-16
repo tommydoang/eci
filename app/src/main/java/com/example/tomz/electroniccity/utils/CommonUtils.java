e6a8500473e882b1f0a73d3275ae486f77fea72b
tommydoang <tommydoang47@gmail.com>
2018-10-16 20:02:58 +0700

    Update CheckHTML, setCurrency

--- app/src/main/java/com/example/tomz/electroniccity/utils/CommonUtils.java
+++ app/src/main/java/com/example/tomz/electroniccity/utils/CommonUtils.java
@@ -2,7 +2,15 @@ package com.example.tomz.electroniccity.utils;
 
 import android.util.Patterns;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CommonUtils {
+
+    private static final String HTML_PATTERN = "<(\"[^\"]*\"|'[^']*'|[^'\">])*>";
+    private static Pattern pattern = Pattern.compile(HTML_PATTERN);
 
     private CommonUtils(){
         // This utility class is not publicly instantiable
@@ -10,6 +18,18 @@ public class CommonUtils {
 
     public static boolean isEmailValid(String email) {
         return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    public static String setCustomCurrency(String price){
+        String formattedCurr;
+        Locale localeID = new Locale("in", "ID");
+        NumberFormat currencyFormat = NumberFormat.getInstance(localeID);
+        formattedCurr = "Rp " + currencyFormat.format(Double.parseDouble(price));
+        return formattedCurr;
+    }
+
+    public static boolean hasHTMLTags(String text){
+        return pattern.matcher(text).find();
     }
 
 }
