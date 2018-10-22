d4f9f76858e5524025cabdb57cb7984654d8c431
tommydoang <tommydoang47@gmail.com>
2018-10-22 13:40:37 +0700

    Clean up unused method

--- app/src/main/java/com/example/tomz/electroniccity/page/main/MainNavigator.java
+++ app/src/main/java/com/example/tomz/electroniccity/page/main/MainNavigator.java
@@ -1,18 +1,9 @@
 package com.example.tomz.electroniccity.page.main;
 
-import com.example.tomz.electroniccity.data.model._testdoang.DataModelIdeas;
-
-import java.util.List;
-
 /**
  * Created by tomz on 01/17/17.
  */
 
 public interface MainNavigator {
-
-    void onSuccessGetAuthKey(String status);
-
     void handleError(Throwable throwable);
-
-    void updateBlog(List<DataModelIdeas> dataModelIdeasList);
 }
