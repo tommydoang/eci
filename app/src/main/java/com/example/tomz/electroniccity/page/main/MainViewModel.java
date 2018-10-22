5ae34cf03b8b650fe62bcd388dca3aa3f0602c6b
tommydoang <tommydoang47@gmail.com>
2018-10-22 13:38:57 +0700

    Update & CleanUp

--- app/src/main/java/com/example/tomz/electroniccity/page/main/MainViewModel.java
+++ app/src/main/java/com/example/tomz/electroniccity/page/main/MainViewModel.java
@@ -1,7 +1,5 @@
 package com.example.tomz.electroniccity.page.main;
 
-import android.util.Log;
-
 import com.example.tomz.electroniccity.data.DataManager;
 import com.example.tomz.electroniccity.page.bn_tab_home.account.AuthRequest;
 import com.example.tomz.electroniccity.utils.base.BaseViewModel;
@@ -16,7 +14,7 @@ public class MainViewModel extends BaseViewModel<MainNavigator> {
         super(dataManager, schedulerProvider);
     }
 
-    public void getAuthApiCall(String key){
+    void getAuthApiCall(String key){
         setIsLoading(true);
         getCompositeDisposable().add(getDataManager()
                 .doAuthApiCall(new AuthRequest.req(key))
@@ -24,14 +22,11 @@ public class MainViewModel extends BaseViewModel<MainNavigator> {
                 .observeOn(getSchedulerProvider().ui())
                 .subscribe(jsonObject -> {
                     JSONObject object = jsonObject.getJSONObject("response");
-                    Log.d("accept tes1 3", "MAIN " +object.getString("divice_id"));
                     getDataManager().authInfo(
                             object.getString("divice_id"),
                             object.getString("token"));
                     setIsLoading(false);
-                    getNavigator().onSuccessGetAuthKey(jsonObject.getString("status"));
                 }, throwable -> {
-                    Log.e("err accept tes1", "MAIN " + throwable.getMessage());
                     setIsLoading(false);
                     getNavigator().handleError(throwable);
                 } )
