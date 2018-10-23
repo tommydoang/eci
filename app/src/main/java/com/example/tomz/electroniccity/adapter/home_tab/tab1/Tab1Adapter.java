279367007915f1d2dbdcaad2760f7054d5361a20
tommydoang <tommydoang47@gmail.com>
2018-10-23 18:47:07 +0700

    Update w/ special price

--- app/src/main/java/com/example/tomz/electroniccity/adapter/home_tab/tab1/Tab1Adapter.java
+++ app/src/main/java/com/example/tomz/electroniccity/adapter/home_tab/tab1/Tab1Adapter.java
@@ -1,16 +1,22 @@
 package com.example.tomz.electroniccity.adapter.home_tab.tab1;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tomz.electroniccity.R;
 import com.example.tomz.electroniccity.data.model.api.products.tab1.DataProductTab1Response;
 import com.example.tomz.electroniccity.databinding.Tab1ItemBestSellerBinding;
 import com.example.tomz.electroniccity.page.bn_home_content.tab1.Tab1ItemViewModel;
+import com.example.tomz.electroniccity.utils.CommonUtils;
 import com.example.tomz.electroniccity.utils.base.BaseViewHolder;
+import com.example.tomz.electroniccity.utils.font.CustomTextViewLatoFont;
 
 import java.util.List;
 
@@ -49,6 +55,7 @@ public class Tab1Adapter extends RecyclerView.Adapter<BaseViewHolder> {
 
         private Tab1ItemBestSellerBinding binding;
         private Tab1ItemViewModel tab1ItemViewModel;
+        private CustomTextViewLatoFont tvNormalPrice, tvSpcPrice, tvDiscount;
 
         BindingHolder(Tab1ItemBestSellerBinding binding) {
             super(binding.getRoot());
@@ -57,6 +64,22 @@ public class Tab1Adapter extends RecyclerView.Adapter<BaseViewHolder> {
 
         @Override
         public void onBind(int position) {
+            tvSpcPrice = binding.tvSpcPrice;
+            tvNormalPrice = binding.tvNormalPrice;
+            tvDiscount = binding.tvDiscount;
+            if (!mTab1Model.get(position).getSpc_price().isEmpty()){
+                tvDiscount.setTextColor(Color.RED);
+                tvNormalPrice.setTextColor(Color.RED);
+                tvNormalPrice.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11);
+                tvDiscount.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11);
+                tvDiscount.setText(String.valueOf((int)CommonUtils.countToDisc(mTab1Model.get(position).getReal_price(),
+                        mTab1Model.get(position).getSpc_price())+"%"));
+                tvNormalPrice.setPaintFlags(tvNormalPrice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            } else {
+                tvNormalPrice.setTextColor(Color.GRAY);
+                tvSpcPrice.setVisibility(View.GONE);
+            }
+
             DataProductTab1Response dmi = mTab1Model.get(position);
             tab1ItemViewModel = new Tab1ItemViewModel(dmi);
             binding.setTab1(tab1ItemViewModel);
